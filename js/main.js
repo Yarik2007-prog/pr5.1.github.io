259555c96a5eebf87d8a912ba0729f0067af8248
Yarik2007-prog <150480330+Yarik2007-prog@users.noreply.github.com>
2026-01-06 15:56:28 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // ===== DOM =====
+  const authBtn = document.querySelector('.button-auth');
+  const cartBtn = document.getElementById('cart-button');
+
+  const modalAuth = document.querySelector('.modal-auth');
+  const closeAuth = document.querySelector('.close-auth');
+  const loginForm = document.getElementById('logInForm');
+
+  const loginInput = document.getElementById('login');
+  const passwordInput = document.getElementById('password');
+
+
+  const openAuthModal = () => {
+    modalAuth.style.display = 'flex';
+  };
+
+  const closeAuthModal = () => {
+    modalAuth.style.display = 'none';
+    clearErrors();
+  };
+
+  const setError = (input) => {
+    input.style.border = '2px solid red';
+  };
+
+  const clearError = (input) => {
+    input.style.border = '';
+  };
+
+  const clearErrors = () => {
+    clearError(loginInput);
+    clearError(passwordInput);
+  };
+
+ 
+  const savedLogin = localStorage.getItem('login');
+
+  if (savedLogin && authBtn) {
+    authBtn.querySelector('.button-text').textContent = savedLogin;
+  }
+
+  // ===== events =====
+  if (authBtn) {
+  authBtn.addEventListener('click', () => {
+    const isLogged = localStorage.getItem('login');
+
+    if (!isLogged) {
+      openAuthModal();
+    } 
+
+    else {
+      localStorage.removeItem('login');
+      authBtn.querySelector('.button-text').textContent = 'Війти';
+    }
+  });
+}
+
+  if (closeAuth) {
+    closeAuth.addEventListener('click', closeAuthModal);
+  }
+
+  loginInput.addEventListener('input', () => clearError(loginInput));
+  passwordInput.addEventListener('input', () => clearError(passwordInput));
+
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const login = loginInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    let hasError = false;
+
+    if (!login) {
+      setError(loginInput);
+      hasError = true;
+    }
+
+    if (!password) {
+      setError(passwordInput);
+      hasError = true;
+    }
+
+    if (hasError) return;
+
+    localStorage.setItem('login', login);
+
+    if (authBtn) {
+      authBtn.querySelector('.button-text').textContent = login;
+    }
+
+    loginInput.value = '';
+    passwordInput.value = '';
+    closeAuthModal();
+  });
+
+});
